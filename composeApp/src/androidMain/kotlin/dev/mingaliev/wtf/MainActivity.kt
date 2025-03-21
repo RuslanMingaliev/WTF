7e92ea1d8dd882db44d8a56ea870a65269d5683c
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2025-03-21 21:47:24 +0700

    Feat: Add Android Edge to Edge support
    Signed-off-by: Ruslan Mingaliev <mingaliev.rr@gmail.com>

--- composeApp/src/androidMain/kotlin/dev/mingaliev/wtf/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/mingaliev/wtf/MainActivity.kt
@@ -1,12 +1,16 @@
 package dev.mingaliev.wtf
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT))
         setContent {
             App()
         }
